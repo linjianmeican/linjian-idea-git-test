47a3f436e5af75a639ab566019dd704205a47ab5
linjianmeican <112602478+linjianmeican@users.noreply.github.com>
2022-09-01 17:51:05 +0800

    Update Test.java

--- src/com/duoke360/Test.java
+++ src/com/duoke360/Test.java
@@ -3,6 +3,7 @@ package com.duoke360;
 public class Test {
     public static void main(String[] args) {
         System.out.println("master v1");
-        System.out.println("github modify");
+        System.out.println("github modify_001");
+        System.out.println("github modify_002");
     }
 }
